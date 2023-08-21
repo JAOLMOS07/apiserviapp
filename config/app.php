8587c2e988f5829b4d5e11c79703604edf097afe
JAOLMOS07 <jhogerolmos10@gmail.com>
2023-08-21 12:58:24 -0500

    atenticación jwt

--- config/app.php
+++ config/app.php
@@ -183,6 +183,9 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+         //Customs
+         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ])->toArray(),
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,25 @@
 <?php
-
+use App\Http\Controllers\V1\ProductsController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    //Prefijo V1, todo lo que este dentro de este grupo se accedera escribiendo v1 en el navegador, es decir /api/v1/*
+    Route::post('login', [AuthController::class, 'authenticate']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::group(['middleware' => ['jwt.verify']], function() {
+        //Todo lo que este dentro de este grupo requiere verificación de usuario.
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('get-user', [AuthController::class, 'getUser']);
+    });
 });
